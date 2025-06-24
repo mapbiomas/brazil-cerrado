d778e17eb82417b73a86fa07472174ce0213bb3b
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-06-24 13:44:04 -0300

    Update 02_computeProportion.js

--- lulc_30m_landsat/collection_100/1-general-map/02_computeProportion.js
+++ lulc_30m_landsat/collection_100/1-general-map/02_computeProportion.js
@@ -1,90 +1,82 @@
-// -- -- -- -- 02_computeProportion
-// compute area by ecoregion to be used as reference to estimate samples distribution 
-// dhemerson.costa@ipam.org.br and barbara.silva@ipam.org.br
+// --- --- --- 02_computeProportion
+/* Compute area per class per ecoregion to support proportional sample distribution
+This script computes the area (in square kilometers) occupied by selected land use and cover classes within each ecoregion of the Cerrado biome for a given reference year. 
+The resulting table is used to estimate the proportional distribution of training samples for classification tasks.
+*/
+// Author: barbara.silva@ipam.org.br
 
-// Input metadata
+// Define output path and version
 var version = '1';
+var dirout = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/LANDSAT/sample/area';
 
-// Define classes to be assessed
+// Define target LULC classes to compute
 var classes = [3, 4, 11, 12, 15, 18, 25, 33];
 
-// Output directory
-var dirout = 'users/dh-conciani/collection9/sample/area';
-
-// Cerrado classification regions
+// Classification regions of the Cerrado biome
 var regionsCollection = ee.FeatureCollection('users/dh-conciani/collection7/classification_regions/vector_v2');
 
-// Set option (avaliable are 'year' or 'stable')
-var option = 'year' ; 
+// Set reference year (default = middle of time series)
+var year = '2005';
 
-// If option equal to year
-if (option == 'year') {
-  
-  // Define year to be used as reference (default: mid of the time-series [nYear/2])
-  var year = '2005';
-  
-  // Load collection 8.0 
-  var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1')
-    .select('classification_' + year);
-}
+// Load MapBiomas Collection 9.0 and select the reference year
+var mapbiomas = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1')
+                 .select('classification_' + year);
 
-if (option == 'stable') {
-  var mapbiomas = ee.Image('users/dh-conciani/collection9/masks/cerrado_trainingMask_1985_2022_v4');
-}
-
-// Define function to compute area (skm)
-var pixelArea = ee.Image.pixelArea().divide(1000000); //km²
-
-// Reclassify collection by ipam-workflow classes 
+// Reclassify original MapBiomas classes to IPAM workflow schema
 mapbiomas = mapbiomas.remap({
-  'from': [3, 4, 5, 6, 49, 11, 12, 32, 29, 50, 13, 15, 19, 39, 20, 40, 62, 41, 36, 46, 47, 35, 48, 23, 24, 30, 25, 33, 31],
-  'to':   [3, 4, 3, 3,  3, 11, 12, 12, 25, 12, 12, 15, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 25, 25, 25, 25, 33, 33]
-  }
-);
+  from: [3, 4, 5, 6, 49, 11, 12, 32, 29, 50, 13, 15, 19, 39, 20, 40, 62, 41, 36, 46, 47, 35, 48, 23, 24, 30, 33, 31],
+  to:   [3, 4, 3, 3,  3, 11, 12, 12, 12, 12, 12, 15, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 25, 25, 25, 33, 33]
+});
 
-// Import mapbiomas color schema 
+// Load MapBiomas color palette
 var vis = {
-    min: 0,
-    max: 62,
-    palette:require('users/mapbiomas/modules:Palettes.js').get('classification8')
+  min: 0,
+  max: 62,
+  palette: require('users/mapbiomas/modules:Palettes.js').get('classification8')
 };
 
-//Plot 
-Map.addLayer(mapbiomas, vis, 'Collection ' + year, true);
+// Plot the reference map
+Map.addLayer(mapbiomas, vis, 'MapBiomas ' + year, true);
 
-// Define function to get class area 
-// For each region 
+// Image with pixel area in square kilometers
+var pixelArea = ee.Image.pixelArea().divide(1e6);
+
+// Function to compute class areas per region
 var getArea = function(feature) {
-  
-  // Get classification for the region [i]
-  var mapbiomas_i = mapbiomas.clip(feature);
-  
-  // For each class [j]
-  classes.forEach(function(class_j) {
-    
-    // Create the reference area
-    var reference_ij = pixelArea.mask(mapbiomas_i.eq(class_j));
-    
-    // Compute area and insert as metadata into the feature 
-    feature = feature.set(String(class_j),
-                         ee.Number(reference_ij.reduceRegion({
-                                      reducer: ee.Reducer.sum(),
-                                      geometry: feature.geometry(),
-                                      scale: 30,
-                                      maxPixels: 1e13 }
-                                    ).get('area')
-                                  )
-                              ); // end of set
-                          }); // end of class_j function
-  // return feature
+  // Clip classification to the region
+  var clipped = mapbiomas.clip(feature);
+
+  // Loop over target classes
+  classes.forEach(function(classId) {
+    // Mask pixels of the given class
+    var classMask = pixelArea.mask(clipped.eq(classId));
+
+    // Compute area (km²) and set as property
+    var area_km2 = classMask.reduceRegion({
+      reducer: ee.Reducer.sum(),
+      geometry: feature.geometry(),
+      scale: 30,
+      maxPixels: 1e13
+    }).get('area');
+
+    // Store rounded value in feature
+    feature = feature.set(
+      String(classId),
+      ee.Number(area_km2).multiply(10000).round().divide(10000) // 4 decimal precision
+    );
+  });
+
   return feature;
-}; 
+};
 
-var computed_obj = regionsCollection.map(getArea);
-print ('Result: ', computed_obj);
-Map.addLayer(computed_obj, {}, 'Result', false);
+// Apply area computation to all regions
+var computed = regionsCollection.map(getArea);
+print('Computed area per class and region:', computed);
+Map.addLayer(computed, {}, 'Area by region', false);
 
-// Export computation as GEE asset
-Export.table.toAsset({'collection': computed_obj, 
-                      'description': year + '_v' + version,
-                      'assetId': dirout + '/' + year + '_v' + version});
+// Export as GEE asset
+Export.table.toAsset({
+  collection: computed,
+  description: year + '_v' + version,
+  assetId: dirout + '/' + year + '_v' + version
+});
