007f518dc1508eeb10d9d194f08c09551ff50dfc
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-06-26 17:07:00 -0300

    Update 06_gapFill.js

--- lulc_30m_landsat/collection_100/1-general-map/06_gapFill.js
+++ lulc_30m_landsat/collection_100/1-general-map/06_gapFill.js
@@ -1,5 +1,4 @@
 // -- -- -- -- 06_gapFill
-
 // Temporal gap-filling of classification maps using previous and next valid years
 // Description: This script applies a temporal gap-filling filter to a time series of classified images (1985–2024), 
 // filling NoData pixels using valid observations from previous or subsequent years.
