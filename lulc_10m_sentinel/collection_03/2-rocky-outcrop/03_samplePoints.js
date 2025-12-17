69c83595534713f395daab238b7d50ef7dea2f40
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-12-17 18:35:11 -0300

    Update 03_samplePoints.js

--- lulc_10m_sentinel/collection_03/2-rocky-outcrop/03_samplePoints.js
+++ lulc_10m_sentinel/collection_03/2-rocky-outcrop/03_samplePoints.js
@@ -2,25 +2,25 @@
 // sort stratified spatialPoints by region using stable pixels
 // barbara.silva@ipam.org.br
 
-// Define string to use as metadata
-var version = '3';  // label string
+// Define metadata
+var version = '1';
 
 // Reference proportion
-var file_in = ee.FeatureCollection('projects/barbaracosta-ipam/assets/collection-9_rocky-outcrop/sample/area/stable_v2');
+var file_in = ee.FeatureCollection('projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/sample/area/stable_v1');
 
 // Read area of interest
-var aoi_vec = ee.FeatureCollection('projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/masks/aoi_v1').geometry();
+var aoi_vec = ee.FeatureCollection('projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/masks/aoi_v1').geometry();
 var aoi_img = ee.Image(1).clip(aoi_vec);
 Map.addLayer(aoi_img, {palette:['red']}, 'Area of Interest');
 
 // Define output
-var output = 'projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/sample/points/';
+var output = 'projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/sample/points/';
 
 // Define classes to generate samples
-var classes = [1, 2, 29];
+var classes = [1, 2, 3, 4, 5];
 
 // Rocky outcrop samples
-var rocky_samples = ee.FeatureCollection('projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/sample/rocky-outcrop-collected')
+var rocky_samples = ee.FeatureCollection('projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/C03_rocky-outcrop-collected-v2')
         // Insert reference class [29] following the mapbiomas schema
         .map(function(feature) {
           return feature.set({'class': '29'}).select(['class']);
@@ -30,11 +30,11 @@ var rocky_samples = ee.FeatureCollection('projects/barbaracosta-ipam/assets/coll
 var sampleSize = 13000;    
 var nSamplesMin = rocky_samples.size().round(); 
 
-// Stable pixels from collection 8
-var stablePixels = ee.Image('projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/masks/cerrado_rockyTrainingMask_2016_2023_v1')
+// Stable pixels from collection 10.0
+var stablePixels = ee.Image('projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/masks/cerrado_rockyTrainingMask_2016_2024_v1')
                   .rename('class');
      
-// Cerrado biome 
+// Cerrado biome limit
 var regionsCollection =  ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biomas-2019')
                               .filterMetadata('Bioma', 'equals', 'Cerrado');
 
@@ -42,30 +42,37 @@ var regionsCollection =  ee.FeatureCollection('projects/mapbiomas-workspace/AUXI
 var vis = {
     'min': 1,
     'max': 29,
-    'palette': ["32a65e","FFFFB2", "ffaa5f"]
+    'palette': ["32a65e","FFFFB2", "2532e4", "ffaa5f"]
 };
 
 Map.addLayer(stablePixels, vis, 'stable pixels');
 
-// Read the area for each class (from previous step)
+// Read the area for each class (from the previous step)
 var vegetation = ee.Number(file_in.first().get('1'));
-var nonvegetation = ee.Number(file_in.first().get('2'));
-var rocky = ee.Number(file_in.first().get('29'));
+var grassland = ee.Number(file_in.first().get('2'));
+var water = ee.Number(file_in.first().get('3'));
+var farming = ee.Number(file_in.first().get('4'));
+var nonvegetated = ee.Number(file_in.first().get('5'));
 
 // Compute the total area 
 var total = vegetation
-          .add(nonvegetation)
-          .add(rocky);
+          .add(water)
+          .add(grassland)
+          .add(farming)
+          .add(nonvegetated);
 
 // Define the equation to compute the n of samples
 var computeSize = function (number) {
-  return number.divide(total).multiply(sampleSize).round().int16().max(nSamplesMin);
+  var minSamples = sampleSize * 0.02; // 2% do total
+  return number.divide(total).multiply(sampleSize).round().int16().max(minSamples);
 };
-  
+
 // Apply the equation to compute the number of samples
 var n_vegetation = computeSize(ee.Number(vegetation));
-var n_nonvegetation = computeSize(ee.Number(nonvegetation));
-var n_rocky = computeSize(ee.Number(rocky));
+var n_water = computeSize(ee.Number(water));
+var n_grassland = computeSize(ee.Number(grassland));
+var n_farming = computeSize(ee.Number(farming));
+var n_nonvegetated = computeSize(ee.Number(nonvegetated));
 
 // Generate the sample points
 var training = stablePixels.stratifiedSample(
@@ -76,21 +83,12 @@ var training = stablePixels.stratifiedSample(
                                'seed': 1,
                                'geometries': true,
                                'classValues': classes,
-                               'classPoints': [n_vegetation, n_nonvegetation, n_rocky]
+                               'classPoints': [n_vegetation, n_water, n_grassland, n_farming, n_nonvegetated]
                                 }
                               );
 
-// Plot points
-Map.addLayer(training, {}, 'samplePoints');
-
-// Print diagnosis for each class
-print('vegetation', training.filterMetadata('class', 'equals', 1).size());
-print('nonvegetation', training.filterMetadata('class', 'equals', 2).size());
-print('rocky', training.filterMetadata('class', 'equals', 29).size());
-
 // Merge with rocky samples
 training = ee.FeatureCollection(training).merge(rocky_samples);
-print ("training samples", training.first());
 
 // Convert the 'class' column to integers
 var trainingSamplesFixed = training.map(function(feature) {
@@ -98,8 +96,19 @@ var trainingSamplesFixed = training.map(function(feature) {
   return feature.set('class', classValue);
 });
 
+// Plot points
+Map.addLayer(trainingSamplesFixed, {}, 'samplePoints');
+
 // Check if the conversion was done correctly
-print("trainingSamplesFixed", trainingSamplesFixed.first());
+print("trainingSamplesFixed", trainingSamplesFixed.size());
+
+// Print diagnosis for each class
+print('vegetation', trainingSamplesFixed.filterMetadata('class', 'equals', 1).size());
+print('grassland', trainingSamplesFixed.filterMetadata('class', 'equals', 2).size());
+print('water', trainingSamplesFixed.filterMetadata('class', 'equals', 3).size());
+print('farming', trainingSamplesFixed.filterMetadata('class', 'equals', 4).size());
+print('nonvegetated', trainingSamplesFixed.filterMetadata('class', 'equals', 5).size());
+print('rocky', trainingSamplesFixed.filterMetadata('class', 'equals', 29).size());
 
 // Export as GEE asset
 Export.table.toAsset({'collection': trainingSamplesFixed,
