b40c466db9927738e48425b6c6268e921549a444
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-12-17 18:31:05 -0300

    Update 01_trainingMask.js

--- lulc_10m_sentinel/collection_03/2-rocky-outcrop/01_trainingMask.js
+++ lulc_10m_sentinel/collection_03/2-rocky-outcrop/01_trainingMask.js
@@ -1,15 +1,15 @@
 // -- -- -- -- 01_trainingMask
-// generate training mask based in stable pixels from mapbiomas collection 8
+// generate training mask based in stable pixels from mapbiomas collection 10.0
 // barbara.silva@ipam.org.br
 
 // Output version
 var version = '1';
 
 // Set directory for the output file
-var dirout = 'projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/masks/';
+var dirout = 'projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/masks/';
 
 // Set area of interest (AOI)
-var aoi_vec = ee.FeatureCollection("projects/barbaracosta-ipam/assets/collection-2_rocky_outcrop/masks/aoi_v1");
+var aoi_vec = ee.FeatureCollection("projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/masks/aoi_v1");
 
 // Transform AOI into image
 var aoi_img = ee.Image(1).clip(aoi_vec);
@@ -23,14 +23,15 @@ var vis = {
     'palette': ["32a65e","FFFFB2", "ffaa5f"]
 };
 
-// Load mapbiomas collection 9.0
-var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1').updateMask(aoi_img);
+// Load mapbiomas collection 10.0
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_integration_v2')
+                   .updateMask(aoi_img);
 
 // Set function to reclassify collection by native vegetation (1), non‑vegetation (2) and rocky outcrop (29)
 var reclassify = function(image) {
   return image.remap({
-    'from': [3, 4, 5, 6, 49, 11, 12, 32, 29, 50, 13, 15, 19, 39, 20, 40, 62, 41, 46, 47, 35, 48, 9, 21, 23, 24, 30, 25, 33, 31, 27],
-    'to':   [1, 1, 1, 1,  1,  1,  1,  1, 29,  1,  1,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2, 2,  2,  2,  2,  2,  2,  2,  2,  2]
+        'from': [3, 4, 5, 6, 49, 11, 12, 32, 50, 15, 19, 39, 20, 40, 62, 41, 36, 46, 47, 35, 48, 23, 24, 30, 33, 31],
+          'to': [1, 1, 1, 1,  1,  3,  2,  2,  2,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  5,  5,  5,  3,  3],
     }
   );
 };
@@ -42,7 +43,8 @@ var numberOfClasses = function(image) {
 
 // Set years to be processed 
 var years = [ 2016, 2017, 2018, 2019, 
-              2020, 2021, 2022, 2023];
+              2020, 2021, 2022, 2023,
+              2024];
 
 // Remap collection to  native vegetation and non‑vegetation classes 
 var recipe = ee.Image([]);      // build an empty container
@@ -70,12 +72,10 @@ print ('MB stable pixels', stable);
 // Export as GEE asset
 Export.image.toAsset({
     "image": stable,
-    "description": 'cerrado_rockyTrainingMask_2016_2023_v' + version,
-    "assetId": dirout + 'cerrado_rockyTrainingMask_2016_2023_v'+ version,
+    "description": 'cerrado_rockyTrainingMask_2016_2024_v' + version,
+    "assetId": dirout + 'cerrado_rockyTrainingMask_2016_2024_v'+ version,
     "scale": 10,
-    "pyramidingPolicy": {
-        '.default': 'mode'
-    },
+    "pyramidingPolicy": {'.default': 'mode'},
     "maxPixels": 1e13,
     "region": aoi_vec.geometry()
 });
