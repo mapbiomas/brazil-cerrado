ca22567e3dc025c4196ce2ab959eae46cf2a8892
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-12-17 17:46:23 -0300

    Update and rename 14_spatial.js to 12_spatial.js

--- lulc_10m_sentinel/collection_03/1-general-map/12_spatial.js
+++ lulc_10m_sentinel/collection_03/1-general-map/12_spatial.js
@@ -1,6 +1,6 @@
-// -- -- -- -- 14_spatial
+// -- -- -- -- 12_spatial
 // post-processing filter: spatial filter to define a minimum area
-// barbara.silva@ipam.org.br and dhemerson.costa@ipam.org.br
+// barbara.silva@ipam.org.br, dhemerson.costa@ipam.org.br and ana.souza@ipam.org.br
 
 // Import mapbiomas color schema 
 var vis = {
@@ -11,59 +11,20 @@ var vis = {
 };
 
 // Set root directory 
-var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-GENERAL-POST/';
-var out = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-GENERAL-POST/';
+var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/SENTINEL/C03-POST-CLASSIFICATION/';
+var out = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/SENTINEL/C03-POST-CLASSIFICATION/';
 
 // Set metadata
-var inputVersion = '4';
-var outputVersion = '6';
+var inputVersion = '6';
+var outputVersion = '3';
 
 // Define input file
-var inputFile = 'CERRADO_S2-C1_gapfill_v10_segmentation_v10_frequency_v6_temporal_v18_noFalseRegrowth_v6_geomorpho_v'+inputVersion;
+var inputFile = 'CERRADO_C03_gapfill_v9_sandveg_v4_frequency_v7_temporal_v10_falseReg_v11_geom_v'+inputVersion;
 
 // Load classification
 var classificationInput = ee.Image(root + inputFile);
 print('Input classification', classificationInput);
 Map.addLayer(classificationInput, vis, 'Input classification');
-
-// Load another classification
-var version_2 = ee.Image("projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-GENERAL-POST/CERRADO_S2-C1_gapfill_v2_segmentation_v0_frequency_v0_temporal_v0");
-
-// Define the old and new values for reclassification
-var old_values = [3, 4, 11, 12, 15, 18, 25, 33];
-var new_values = [3, 4, 11, 12, 21, 21, 25, 33];
-
-// Apply the reclassification
-old_values.forEach(function(value, i) {
-  version_2 = version_2.where(version_2.eq(value), new_values[i]);
-});
-
-// Define the geometry for non-vegetated area in Araguaia (TO) region
-var geometryNonVeg = ee.Image(1).clip(ee.Geometry.MultiPolygon(
-  [[[-48.88849052214015,-9.62919167826987], 
-    [-48.39959892057765,-9.62919167826987], 
-    [-48.39959892057765,-7.429206092577588], 
-    [-48.88849052214015,-7.429206092577588]]]
-));
-
-// Initialize an empty image to store the reclassified bands
-var remapped = ee.Image([]);
-
-// Iterate over the years and apply the non-vegetated condition
-ee.List.sequence({'start': 2016, 'end': 2023}).getInfo().forEach(function(year_i) {
-  // Select the classification image for the corresponding year
-  var imageYear = classificationInput.select('classification_' + year_i);
-  var versionYear = version_2.select('classification_' + year_i);
-  
-  // Apply the reclassification condition for non-vegetated areas (25)
-  imageYear = imageYear.where(geometryNonVeg.eq(1).and(imageYear.eq(25)), versionYear);
-  
-  // Add the reclassified band to the final filtered image
-  remapped = remapped.addBands(imageYear.updateMask(imageYear.neq(0)));
-});
-
-Map.addLayer(remapped, vis, 'Remapped');
-print('remapped', remapped);
 
 
 // --- ---  Apply 1st sequence of the spatial filter
@@ -71,18 +32,18 @@ print('remapped', remapped);
 var filtered = ee.Image([]);
 
 // Set filter size
-var filter_size = 50;
+var filter_size = 60;
 
 // Iterate over the years
-ee.List.sequence({'start': 2016, 'end': 2023}).getInfo()
+ee.List.sequence({'start': 2017, 'end': 2024}).getInfo()
       .forEach(function(year_i) {
         // Compute the focal model
-        var focal_mode = remapped.select(['classification_' + year_i])
+        var focal_mode = classificationInput.select(['classification_' + year_i])
                 .unmask(0)
                 .focal_mode({'radius': 1, 'kernelType': 'square', 'units': 'pixels'});
  
         // Compute the number of connections
-        var connections = remapped.select(['classification_' + year_i])
+        var connections = classificationInput.select(['classification_' + year_i])
                 .unmask(0)
                 .connectedPixelCount({'maxSize': 100, 'eightConnected': false});
         
@@ -90,7 +51,7 @@ ee.List.sequence({'start': 2016, 'end': 2023}).getInfo()
         var to_mask = focal_mode.updateMask(connections.lte(filter_size));
 
         // Apply filter
-        var classification_i = remapped.select(['classification_' + year_i])
+        var classification_i = classificationInput.select(['classification_' + year_i])
                 .blend(to_mask)
                 .reproject('EPSG:4326', null, 10);
 
@@ -108,7 +69,7 @@ Map.addLayer(filtered, vis, 'filtered - round 1');
 var container = ee.Image([]);
 
 // Iterate over the years
-ee.List.sequence({'start': 2016, 'end': 2023}).getInfo()
+ee.List.sequence({'start': 2017, 'end': 2024}).getInfo()
       .forEach(function(year_i) {
         // Compute the focal model
         var focal_mode = filtered.select(['classification_' + year_i])
@@ -136,12 +97,7 @@ ee.List.sequence({'start': 2016, 'end': 2023}).getInfo()
 // Plot 2nd sequence of the spatial filter
 Map.addLayer(container, vis, 'filtered 2 - round 2');
 
-// Write metadata
-container = container.set('6-spatial', outputVersion)
-                     .copyProperties(classificationInput);
-
 print('Output classification', container);
-
 
 // Export as GEE asset
 Export.image.toAsset({
@@ -155,3 +111,4 @@ Export.image.toAsset({
     'scale': 10,
     'maxPixels': 1e13
 });
+
