094db672870018c00f02e1c6f05bf94395ab5a05
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2025-12-18 09:30:03 -0300

    Update and rename 10_integrate.js to 09_integration.js

--- lulc_10m_sentinel/collection_03/2-rocky-outcrop/09_integration.js
+++ lulc_10m_sentinel/collection_03/2-rocky-outcrop/09_integration.js
@@ -1,15 +1,18 @@
-// --- --- --- 10_integration
+// --- --- --- 09_integration
 // Post-processing: integration of the classification of 'rocky outcrop' (individual flow) with the classification of native vegetation and anthropic class [21]
 // barbara.silva@ipam.org.br and dhemerson.costa@ipam.org.br
 
 // Define input files
-var native = ee.Image('projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-GENERAL-POST/CERRADO_S2-C1_gapfill_v10_seg_v10_frequency_v6_temporal_v18_FalseRegrowth_v6_geomorpho_v4_spatial_v6');
-var rocky = ee.Image('projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-ROCKY-POST/CERRADO_col2_rocky_gapfill_v1_seg_v1_frequency_v4_spatial_v2');
+var native = ee.Image('projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/SENTINEL/C03-POST-CLASSIFICATION/CERRADO_C03_gapfill_v9_sandveg_v4_frequency_v7_temporal_v10_falseReg_v11_geom_v6_spatial_v3');
+var rocky = ee.Image('projects/ee-barbarasilvaipam/assets/collection-03_rocky-outcrop/post-classification/CERRADO_C03_rocky_gapfill_frequency_spatial_v6');
 
 print('Input native vegetation classification', native);
 print('Input rocky outcrop classification', rocky);
 
-// Import MapBiomas color ramp
+// Define output file
+var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO10_DEV/CERRADO/SENTINEL/C03-POST-CLASSIFICATION/';
+
+// Import MapBiomas color schema
 var vis = {
     min: 0,
     max: 62,
@@ -23,41 +26,31 @@ Map.addLayer(rocky, vis, 'Rocky outcrop');
 
 // Define ecoregions layer
 var regions = ee.Image(1).clip(ee.FeatureCollection('users/dh-conciani/collection7/classification_regions/vector_v2'));
-var geometry = regions.geometry();
 
 var scale = 10;
-
-// Avoid the rocky outcrop class overlapping the grassland class in the Chapada dos Veadeiros region
-var geometryVeadeiros = ee.Image(1).clip(ee.FeatureCollection(ee.Geometry.Polygon(
-    [[[-47.91136858164175, -13.828379924704489],
-      [-47.91136858164175, -14.275996243879357],
-      [-47.28377458750113, -14.275996243879357],
-      [-47.28377458750113, -13.828379924704489]]], null, false)));
-
-var mask = regions.where(geometryVeadeiros.eq(1), 4);
 
 // Create a container
 var container = ee.Image([]);
 
 // Integrate layers
-ee.List.sequence({start: 2016, end: 2023}).getInfo().forEach(function(year) {
+ee.List.sequence({start: 2017, end: 2024}).getInfo().forEach(function(year) {
     // Get year-specific images
     var nativeYear = native.select(['classification_' + year]);
     var rockyYear = rocky.select(['classification_' + year]);
     
     // Integrate classifications
-    var integratedYear = nativeYear.where(rockyYear.eq(29).and(mask.neq(4)), 29);
+    var integratedYear = nativeYear.where(nativeYear.neq(3).and(rockyYear.eq(29)), 29);
     
     // Apply a post-integration spatial filter
     // Compute the focal model
     var focalMode = integratedYear
                     .unmask(0)
-                    .focal_mode({radius: 10, kernelType: 'square', units: 'pixels'});
+                    .focal_mode({radius: 2, kernelType: 'square', units: 'pixels'});
 
     // Compute the number of connections
     var connections = integratedYear
                       .unmask(0)
-                      .connectedPixelCount({maxSize: 100, eightConnected: false});
+                      .connectedPixelCount({maxSize: 120, eightConnected: false});
 
     // Get the focal model when the number of connections of the same class is lower than the parameter
     var toMask = focalMode.updateMask(connections.lte(6));
@@ -76,16 +69,12 @@ ee.List.sequence({start: 2016, end: 2023}).getInfo().forEach(function(year) {
 Map.addLayer(container, vis, 'Integrated');
 print('Output integrated classification', container);
 
-var root = 'projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL-ROCKY-POST/';
-
 // Export as GEE asset
 Export.image.toAsset({
     image: container,
-    description: 'CERRADO_col2_native6_rocky2',
-    assetId: root + 'CERRADO_col2_native6_rocky2',
-    pyramidingPolicy: {
-        '.default': 'mode'
-    },
+    description: 'CERRADO_C03_native3_rocky6_v6',
+    assetId: root + 'CERRADO_C03_native3_rocky6_v6',
+    pyramidingPolicy: {'.default': 'mode'},
     region: native.geometry(),
     scale: scale,
     maxPixels: 1e13
