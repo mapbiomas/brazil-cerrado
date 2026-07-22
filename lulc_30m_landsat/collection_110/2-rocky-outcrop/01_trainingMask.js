5c203def4ab9a253264973b8a8666f38742dcf90
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2026-07-21 21:44:39 -0300

    Update 01_trainingMask.js

--- lulc_30m_landsat/collection_110/2-rocky-outcrop/01_trainingMask.js
+++ lulc_30m_landsat/collection_110/2-rocky-outcrop/01_trainingMask.js
@@ -12,27 +12,27 @@ var vis = {
   palette: ["#1f8d49", "#d6bc74", "#2532e4", "#edde8e", "#d4271e"] };
 
 // Define the version  for the output asset
-var version = '1';
+var version = '3';
 
 // Define the base output directory path
-var dirout = 'projects/ee-barbarasilvaipam/assets/collection-04_rocky-outcrop/masks/';
+var dirout = 'projects/ee-barbarasilvaipam/assets/collection-11_rocky-outcrop/masks/';
 
 // Load the Area of Interest (AOI)
-var aoi_vec = ee.FeatureCollection("projects/ee-barbarasilvaipam/assets/collection-04_rocky-outcrop/masks/aoi_v1");
+var aoi_vec = ee.FeatureCollection("projects/ee-barbarasilvaipam/assets/collection-11_rocky-outcrop/masks/aoi_v1");
 
 // Convert the AOI feature collection into a binary image mask for raster operations
 var aoi_img = ee.Image(1).clip(aoi_vec);
 
-// Load the MapBiomas 10m LULC Collection 3.0
-var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc_10m/collection3/mapbiomas_10m_collection3_integration_v1')
+// Load the MapBiomas 10m LULC Collection 10.1
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10_1/mapbiomas_brazil_collection10_1_coverage_v1')
                     .updateMask(aoi_img);
 
 // Define a function to group original LULC classes into broad thematic categories
 var reclassify = function(image) {
-  // Remap to: 1 (Forest), 2 (Herbaceous), 3 (Wetland/Water), 4 (Farming), 5 (Non-Vegetated)
+  // Remap to: forest (1), herbaceous vegetation (2), farming (3), non-vegetated (4) and wetland and water (5)
   return image.remap({
-    'from': [3, 4, 5, 6, 49, 11, 12, 32, 50, 15, 19, 36, 9, 23, 24, 30, 75, 25, 33, 31],
-    'to':   [1, 1, 1, 1,  1,  3,  2,  2,  2,  4,  4,  4,  4,  5,  5,  5,  5,  5,  3,  3]
+    'from': [3, 4, 5, 6, 49, 11, 12, 32, 29, 50, 15, 19, 39, 20, 40, 62, 41, 36, 46, 47, 35, 48, 23, 24, 30, 33, 31],
+      'to': [1, 1, 1, 1,  1,  5,  2,  2, 29,  2,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  5,  5]
   });
 };
 
@@ -43,7 +43,13 @@ var numberOfClasses = function(image) {
 };
 
 // Define the sequence of years to evaluate for pixel stability
-var years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+var years = [ 1985, 1986, 1987, 1988, 1989, 1990, 
+              1991, 1992, 1993, 1994, 1995, 1996, 
+              1997, 1998, 1999, 2000, 2001, 2002, 
+              2003, 2004, 2005, 2006, 2007, 2008, 
+              2009, 2010, 2011, 2012, 2013, 2014,
+              2015, 2016, 2017, 2018, 2019, 2020, 
+              2021, 2022, 2023, 2024];
 
 // Initialize an empty Earth Engine image to accumulate the reclassified annual bands
 var container = ee.Image([]);
@@ -74,9 +80,9 @@ print('MB Stable Pixels', stable);
 // Export as GEE asset
 Export.image.toAsset({
     "image": stable,
-    "description": 'cerrado_rockyTrainingMask_2017_2024_v' + version,
-    "assetId": dirout + 'cerrado_rockyTrainingMask_2017_2024_v'+ version,
-    "scale": 10,
+    "description": 'cerrado_rockyTrainingMask_1985_2024_v' + version,
+    "assetId": dirout + 'cerrado_rockyTrainingMask_1985_2024_v'+ version,
+    "scale": 30,
     "pyramidingPolicy": {'.default': 'mode'},
     "maxPixels": 1e13,
     "region": aoi_vec.geometry()
